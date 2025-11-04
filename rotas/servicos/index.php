0c68e17409ce49861e0aa8b2409530891f34000a
jenxzz <mariaeduardamatoslopes57@gmail.com>
2025-11-04 17:03:53 -0300

    tarefa concluida

---
+++ rotas/servicos/index.php
@@ -0,0 +1,39 @@
+<?php
+header ("Content-Type: application/json");
+header ("Acess-Control-Allow-Origin: *");
+header ("Acess-Control-Allow-Methods: GET, POST, PUT, DELETE");
+
+$conexao = new mysqli ("localhost", "root", "", "AulaSW");
+
+if ($conexao->connect_error) {
+    echo json_encode ("{ 'Conexão Falhou': '".conexao->connect_error."'}");
+}
+
+$MetodoHTTP = $_SERVER ['REQUEST_METHOD'];
+echo $MetodoHTTP;
+
+switch ($MetodoHTTP) {
+    case 'GET':
+        $sql = "Select * from SERVICOS";
+        $resultado = $conexao->query($sql);
+        if ($resultado->num_rows > 0) {
+            $resposta = [];
+            while ($linha = $resultado->fetch_assoc()) {
+                $resposta[] = $linha;
+            }
+            http_response_code(200);
+            echo json_encode($resposta);
+        }
+
+        else {
+            http_response_code(404);
+            echo json_encode('mensagem' => 'Nenhum registro encontrado');
+        }
+
+        break;
+    
+    default:
+        
+        break;
+}
+?>
